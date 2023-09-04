0a53b893f39f67995963f99c2ff4fd2da32a3e56
Daniel Vianna <1708810+dmvianna@users.noreply.github.com>
2023-09-04 15:41:10 +1000

    cons car cdr

---
+++ eintr-7.el
@@ -0,0 +1,28 @@
+;;; Package --- Summary:
+;;;
+;;; Exercises for Emacs Introductory tutorial
+;;;
+;;; Commentary:
+;;;
+;; 7.7 Exercise
+;; ============
+
+;; Construct a list of four birds by evaluating several expressions with
+;; ‘cons’.  Find out what happens when you ‘cons’ a list onto itself.
+;; Replace the first element of the list of four birds with a fish.
+;; Replace the rest of that list with a list of other fish.
+
+;;; Code:
+
+(setq birds (cons "chicken"
+                  (cons "cockatoo"
+                        (cons "condor"
+                              (cons"penguin" ())))))
+
+(cons birds birds)
+
+(setcar birds "shark")
+
+(setcdr birds (list "salmon" "piranha" "anglerfish"))
+
+;;; eintr-7.el ends here
